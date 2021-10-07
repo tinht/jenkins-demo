abe61d18ea7e0e94b21c57aa351a6c993b1fa609
tinht <trantinh.10101991@gmail.com>
2021-10-07 11:48:42 +0700

    Update source code

---
+++ src/main/java/com/example/jenkins/demo/repository/ThemeParkRideRepository.java
@@ -0,0 +1,12 @@
+package com.example.jenkins.demo.repository;
+
+import com.example.jenkins.demo.domain.ThemeParkRide;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ThemeParkRideRepository extends CrudRepository<ThemeParkRide, Long> {
+    List<ThemeParkRide> findByName(String name);
+}
